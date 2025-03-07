8fea81240b73484ffb54c51cd3334878eaeb00c6
pavel khat <khatpavel@gmail.com>
2025-03-07 19:18:30 +0300

    getAll method

--- src/main/java/com/example/consoleApp/Gson/GsonWriterRepositoryImp.java
+++ src/main/java/com/example/consoleApp/Gson/GsonWriterRepositoryImp.java
@@ -3,9 +3,12 @@ package com.example.consoleApp.Gson;
 import com.example.consoleApp.model.Writer;
 import com.example.consoleApp.repository.WriterRepository;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GsonWriterRepositoryImp implements WriterRepository {
@@ -15,17 +18,33 @@ public class GsonWriterRepositoryImp implements WriterRepository {
 
     @Override
     public void save(Writer writer) {
-//        List<Writer> writers = getAll();
+        List<Writer> writers = getAll();
         writers.add(writer);
         writeToFile(writers);
     }
 
 
-    private void writeToFile(List<Writer> writers) {
+    public void writeToFile(List<Writer> writers) {
         try {
             Files.write(Paths.get(file), gson.toJson(writers).getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<Writer> getAll() {
+
+            Path fPath = Paths.get(file);
+
+            if(!Files.exists(fPath)) {
+                return new ArrayList<>();
+            }
+            try {
+                String json = Files.readString(fPath);
+                return gson.fromJson(json, new TypeToken<List<Writer>>() {}.getType());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new ArrayList<>();
+            }
     }
 }
